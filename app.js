4e83069b5fb6b4a30a23485b4da897cffb8b9239
Jethanandj52 <134337163+Jethanandj52@users.noreply.github.com>
2024-11-06 17:11:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function time() {
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    const amPm = hours >= 12 ? 'PM' : 'AM';
+
+   
+    hours = hours % 12 || 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    const timeString = `${hours}:${minutes}:${seconds} ${amPm}`;
+    document.getElementById('clock').textContent = timeString;
+}
+
+ 
+setInterval(time, 1000);
+updateClock();  
